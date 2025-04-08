d7dc26b51d3295c7be422b5e713187242a7de3d2
Guilherme Trevizani <guilhermetrevizani@hotmail.com>
2025-04-08 12:49:37 +0100

    fix: add migration

---
+++ src/TrevizaniRoleplay.Server/DesignTimeDbContextFactory.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using TrevizaniRoleplay.Infra.Data;
+
+namespace TrevizaniRoleplay.Server;
+
+public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<DatabaseContext>
+{
+    public DatabaseContext CreateDbContext(string[] args)
+    {
+        return Functions.GetDatabaseContext();
+    }
+}
